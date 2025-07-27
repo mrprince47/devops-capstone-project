d667c293f6a4f346d1ee4ca2b57e4665f526ee7a
mrprince47 <vswami7829@gmail.com>
2025-07-27 17:38:11 -0400

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -14,6 +14,7 @@ from service.models import db, Account, init_db
 from service.routes import app
 from service import talisman
 
+HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
@@ -182,15 +183,22 @@ class TestAccountService(TestCase):
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-        def test_security_headers(self):
-            """It should return security headers"""
-            response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
-            headers = {
+    def test_security_headers(self):
+        """It should return security headers"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        headers = {
             'X-Frame-Options': 'SAMEORIGIN',
             'X-Content-Type-Options': 'nosniff',
             'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
             'Referrer-Policy': 'strict-origin-when-cross-origin'
-            }
-            for key, value in headers.items():
-                self.assertEqual(response.headers.get(key), value)
+        }
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
+
+    def test_cors_security(self):
+        """It should return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
